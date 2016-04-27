585b52a25a3b8c55cde4b442f0fbc136d94bee5b
Igor3Volf <igor3volf@mail.ru>
2016-04-27 00:55:23 -0400

    adduser and deleteuser
    check the adduser control and the deleteuser loop in view im not sure if
    they work cuz i dont know if i can access db

--- StudioBookingApp/BookingApp/controller/admin.php
+++ StudioBookingApp/BookingApp/controller/admin.php
@@ -26,12 +26,32 @@ class Admin extends Controller
     {
         if(isset(Session::get("my_user")['id']))
         {
-            $this->view('admin/Adduser', ['first_name' => Session::get("my_user")['first_name'], 'last_name' => Session::get("my_user")['last_name']  ] );
+            $this->view('admin/Adduser', ['first_name' => Session::get("my_user")['first_name'], 'last_name' => Session::get("my_user")['last_name']] );
+            
         }
         else
         {
             $this->view('admin/Adduser', []);
         }
+        
+        if(isset($_POST['adduser']))
+        {
+            // i guess this is the salt :X
+            require_once ('../BookingApp/core/Hash.php');
+            $salt = Hash::createSalt();
+            $hashedpw = Hash::hashPassword($password,$salt);
+            
+            $user = $this->model('User');
+            $st = $this->db->insert('users',[ 'u_name' => $_POST['uname'] ,
+                                                'u_first' => $_POST['fname'],
+                                                'u_last' => $_POST['lname'],
+                                                'u_pass' => $_POST['password'],
+                                                'u_email' => $_POST['email'],
+                                                'u_salt' => $salt, // idk where to get the salt from
+                                                'login_ip' => $_SERVER['REMOTE_ADDR'] ]);
+            
+        }
+        
     }
 
     public function deleteuser()
@@ -44,5 +64,8 @@ class Admin extends Controller
         {
             $this->view('admin/Deleteuser', []);
         }
+        
+        
+        
     }
 }
