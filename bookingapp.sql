6831c4f75feb0fead9e0a3d3d078f9993d3fc319
Igor3Volf <igor3volf@mail.ru>
2016-04-26 22:33:23 -0400

    Full DB
    all tables are ready

--- bookingapp.sql
+++ bookingapp.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 26, 2016 at 07:15 AM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.5.34
+-- Generation Time: Apr 27, 2016 at 04:31 AM
+-- Server version: 10.1.10-MariaDB
+-- PHP Version: 7.0.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,6 +19,139 @@ SET time_zone = "+00:00";
 --
 -- Database: `bookingapp`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `availabilities`
+--
+
+CREATE TABLE `availabilities` (
+  `a_id` int(3) NOT NULL,
+  `a_start` date DEFAULT NULL,
+  `a_end` date DEFAULT NULL,
+  `a_type` enum('Daily','Weekly') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `billing`
+--
+
+CREATE TABLE `billing` (
+  `b_id` int(8) NOT NULL,
+  `u_id` int(5) NOT NULL,
+  `b_fname` varchar(30) NOT NULL,
+  `b_lname` varchar(30) NOT NULL,
+  `b_address` varchar(50) NOT NULL,
+  `b_apt_num` varchar(7) NOT NULL,
+  `b_city` varchar(25) NOT NULL,
+  `b_state` varchar(30) NOT NULL,
+  `b_country` varchar(30) NOT NULL,
+  `b_zip` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `files`
+--
+
+CREATE TABLE `files` (
+  `f_id` int(7) NOT NULL,
+  `f_location` varchar(255) NOT NULL,
+  `f_mime_type` varchar(30) NOT NULL,
+  `u_id` int(5) NOT NULL,
+  `f_uploaded_by` int(5) NOT NULL,
+  `f_upload_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `p_id` int(8) NOT NULL,
+  `p_status` int(1) NOT NULL,
+  `p_method` int(1) NOT NULL,
+  `b_id` int(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_method`
+--
+
+CREATE TABLE `payment_method` (
+  `m_id` int(1) NOT NULL,
+  `m_name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_status`
+--
+
+CREATE TABLE `payment_status` (
+  `ps_id` int(1) NOT NULL,
+  `ps_name` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reset_password`
+--
+
+CREATE TABLE `reset_password` (
+  `r_id` int(8) NOT NULL,
+  `u_id` int(5) NOT NULL,
+  `expiration` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `token` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rules`
+--
+
+CREATE TABLE `rules` (
+  `r_id` int(3) NOT NULL,
+  `r_min_payment` decimal(5,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
+
+CREATE TABLE `sessions` (
+  `s_id` int(8) NOT NULL,
+  `s_start` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `s_end` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `u_id` int(5) NOT NULL,
+  `s_type` int(1) NOT NULL,
+  `s_desc` varchar(255) NOT NULL,
+  `p_id` int(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `session_type`
+--
+
+CREATE TABLE `session_type` (
+  `t_id` int(1) NOT NULL,
+  `t_name` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -49,6 +182,66 @@ INSERT INTO `users` (`u_id`, `u_name`, `u_first`, `u_last`, `u_pass`, `u_salt`, 
 --
 
 --
+-- Indexes for table `availabilities`
+--
+ALTER TABLE `availabilities`
+  ADD PRIMARY KEY (`a_id`);
+
+--
+-- Indexes for table `billing`
+--
+ALTER TABLE `billing`
+  ADD PRIMARY KEY (`b_id`);
+
+--
+-- Indexes for table `files`
+--
+ALTER TABLE `files`
+  ADD PRIMARY KEY (`f_id`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`p_id`);
+
+--
+-- Indexes for table `payment_method`
+--
+ALTER TABLE `payment_method`
+  ADD PRIMARY KEY (`m_id`);
+
+--
+-- Indexes for table `payment_status`
+--
+ALTER TABLE `payment_status`
+  ADD PRIMARY KEY (`ps_id`);
+
+--
+-- Indexes for table `reset_password`
+--
+ALTER TABLE `reset_password`
+  ADD PRIMARY KEY (`r_id`);
+
+--
+-- Indexes for table `rules`
+--
+ALTER TABLE `rules`
+  ADD PRIMARY KEY (`r_id`);
+
+--
+-- Indexes for table `sessions`
+--
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`s_id`);
+
+--
+-- Indexes for table `session_type`
+--
+ALTER TABLE `session_type`
+  ADD PRIMARY KEY (`t_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -58,6 +251,56 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `availabilities`
+--
+ALTER TABLE `availabilities`
+  MODIFY `a_id` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `billing`
+--
+ALTER TABLE `billing`
+  MODIFY `b_id` int(8) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `files`
+--
+ALTER TABLE `files`
+  MODIFY `f_id` int(7) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `p_id` int(8) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `payment_method`
+--
+ALTER TABLE `payment_method`
+  MODIFY `m_id` int(1) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `payment_status`
+--
+ALTER TABLE `payment_status`
+  MODIFY `ps_id` int(1) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `reset_password`
+--
+ALTER TABLE `reset_password`
+  MODIFY `r_id` int(8) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `rules`
+--
+ALTER TABLE `rules`
+  MODIFY `r_id` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `sessions`
+--
+ALTER TABLE `sessions`
+  MODIFY `s_id` int(8) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `session_type`
+--
+ALTER TABLE `session_type`
+  MODIFY `t_id` int(1) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `users`
 --
